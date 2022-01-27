f96093326921ecd2d7b07ab4f20c0ab0e680c647
Sergey Levin <sergey.levin@rokolabs.com>
2022-01-27 18:30:39 +0400

    Add reports

--- cucumber.js
+++ cucumber.js
@@ -1,19 +1,28 @@
+/* eslint-disable prettier/prettier */
 var commonArrayParams = [
     '--require-module ts-node/register',
     '--require src/**/*.ts',
     '--format-options \'{ "snippetInterface": "async-await" }\'',
     '--publish-quiet',
+    '-f json:reports/cucumber_report.json', // Note: this formatter is deprecated and will be removed in the next major release.
+    '-f message:reports/cucumber-messages.ndjson'
 ];
 
 var defaultParams = commonArrayParams.concat(['--format progress-bar']).join(' ');
 
-var ciParams = commonArrayParams.concat(['--format progress']).join(' ');
+var ciParams = commonArrayParams.concat([]).join(' ');
 
 var checkParams = commonArrayParams.concat([' --format summary']).join(' ');
 
 var debugParams = commonArrayParams.concat(['--tags @debug', ' --format cucumber-console-formatter']).join(' ');
 
-var tagOnlyParams = commonArrayParams.concat(['--tags @only', ' --format cucumber-console-formatter']).join(' ');
+var tagOnlyParams = commonArrayParams
+    .concat([
+        '--tags @only',
+        ' --format cucumber-console-formatter',
+        ' -f html:reports/build_in_report.html'
+    ])
+    .join(' ');
 
 var quickRunParams = commonArrayParams
     .concat(['--format cucumber-console-formatter', '--format-options \'{ "colorsEnabled": false }\''])
